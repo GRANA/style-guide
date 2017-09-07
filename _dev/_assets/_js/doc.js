43c817fce0a91d0839f4116dcc5790b71dd53dd2
SeptimusPrime <adam@grana.com>
2017-09-07 13:59:48 +0800

    M2-1713-site Ability to add id to section to enable hash tag to scroll feature

--- _dev/_assets/_js/doc.js
+++ _dev/_assets/_js/doc.js
@@ -6,10 +6,15 @@ var granaDocs = (function($) {
       $.fieldText();
       hljs.initHighlightingOnLoad();
     });
+
+    $(window).load(function() {
+      $('.doc-content').scrollTop($(window.location.hash).position().top);
+    });
+
   };
 
   return {
-  	init: init,
+    init: init,
   };
 })(jQuery);
 
