44a5c312609315975bb7ee6f48132cb4f84fc0cf
SeptimusPrime <adam@grana.com>
2018-06-01 17:51:57 +0800

    react Add empty check

--- core/dialectics/js/src/fieldtext.jsx
+++ core/dialectics/js/src/fieldtext.jsx
@@ -3,11 +3,13 @@ class FieldText extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { 
-			isFocus: false
+			isFocus: false,
+			isFilled: false
 		};
 
 		this.onBlur = this.onBlur.bind(this);
 		this.onFocus = this.onFocus.bind(this);
+		this.checkEmpty = this.checkEmpty.bind(this);
 	}
 
 	onBlur() {
@@ -18,15 +20,26 @@ class FieldText extends React.Component {
 		this.setState({isFocus: true});
 	}
 
+	checkEmpty(evt) {
+		var value = evt.target.value;
+
+		if (!value) {
+			this.setState({isFilled: false});
+		} else {
+			this.setState({isFilled: true});
+		}
+    }
+
 	render() {
 		var classes = classNames([
-			this.state.isFocus && 'is-focus'
+			this.state.isFocus && 'is-focus',
+			this.state.isFilled && 'is-filled'
 	    ]);
 
 		return (
 			<div className={'field-group ' + classNames(classes)}>
 				<label htmlFor={this.props.name} className="field-label">{this.props.label}</label>
-				<input type="text" id={this.props.name} onBlur={this.onBlur} onFocus={this.onFocus} className="field-text" name={this.props.name}/>
+				<input type="text" id={this.props.name} onBlur={this.onBlur} onFocus={this.onFocus} onKeyUp={this.checkEmpty} onPaste={this.checkEmpty} className="field-text" name={this.props.name}/>
 				<div className="field-helper">hint and helper text</div>
 			</div>
 		);
